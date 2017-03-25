0d92691b31714938edc3490f96ce3ff42e90f7c3
nezharen <nezharen1995@gmail.com>
2017-03-25 21:09:57 +0800

    system tray icon

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,7 +1,37 @@
+#include <QCloseEvent>
 #include "MainWindow.h"
 
 MainWindow::MainWindow()
 {
+	systemTrayIcon = new QSystemTrayIcon(this);
+	connect(systemTrayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(systemTrayIconActivated(QSystemTrayIcon::ActivationReason)));
+	systemTrayIcon->show();
 	setWindowTitle(tr("ZKStock"));
 	show();
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+	if (systemTrayIcon->isVisible())
+	{
+		hide();
+		event->ignore();
+	}
+	else
+		event->accept();
+}
+
+void MainWindow::systemTrayIconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+	switch (reason)
+	{
+	case QSystemTrayIcon::Trigger:
+		if (isVisible())
+			hide();
+		else
+			show();
+		break;
+	default:
+		break;
+	}
 }
--- MainWindow.h
+++ MainWindow.h
@@ -2,13 +2,19 @@
 #define MAIN_WINDOW_H
 
 #include <QMainWindow>
+#include <QSystemTrayIcon>
 
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 public:
 	MainWindow();
+protected:
+	void closeEvent(QCloseEvent *event);
+private slots:
+	void systemTrayIconActivated(QSystemTrayIcon::ActivationReason reason);
 private:
+	QSystemTrayIcon *systemTrayIcon;
 };
 
 #endif
